07ebcaff7f33c65ce1629a105fdf1a244d163c53
mshibl <mohamed.shibl@hotmail.com>
2016-01-21 22:52:51 -0800

    adds user log in feature

--- simple-todos.js
+++ simple-todos.js
@@ -31,7 +31,9 @@ if (Meteor.isClient) {
       // Insert a task into the collection
       Tasks.insert({
         text: text,
-        createdAt: new Date() // current time
+        createdAt: new Date(), // current time
+        owner: Meteor.userId(),
+        username: Meteor.user().username
       });
  
       // Clear form
@@ -52,5 +54,9 @@ if (Meteor.isClient) {
     "click .delete": function () {
       Tasks.remove(this._id);
     }
+  });
+
+  Accounts.ui.config({
+    passwordSignupFields: "USERNAME_ONLY"
   });
 }
